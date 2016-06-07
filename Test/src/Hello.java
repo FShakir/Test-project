bb8f21fc8645cb166a4a1a17b489274a0a9dbee5
FShakir <Fshaki3@uic.edu>
2016-06-07 15:40:14 -0700

    Lost

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -3,6 +3,9 @@ public class Hello {
 	public static void main (String [] args){
 System.out.println("Hello World");
 System.out.println("Update Test");
+<<<<<<< HEAD
 
+=======
+>>>>>>> refs/heads/Test-Branch
 	}
 }

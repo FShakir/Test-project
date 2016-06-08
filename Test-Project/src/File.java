84964d2dbed498fbcb8cea92e53bb3b39e94c76a
FShakir <Fshaki3@uic.edu>
2016-06-08 09:38:01 -0700

    Update File.java

--- Test-Project/src/File.java
+++ Test-Project/src/File.java
@@ -4,16 +4,7 @@ public class File {
 	public static void main(String[] args) {
 		System.out.println("First Message");
 		System.out.println("Second Message");
-		System.out.println("First Message");
-		System.out.println("Second Message");
-		System.out.println("First Message");
-		System.out.println("Second Message");
-		System.out.println("First Message");
-		System.out.println("Second Message");
-		System.out.println("First Message");
-		System.out.println("Second Message");
-		System.out.println("First Message");
-		System.out.println("Second Message");
+
 		
 	}
 
